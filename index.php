48ce0e7fd24f34fa7e599323de56475f9ea0ea19
kjurassic <kjuresich@gmail.com>
2021-09-10 16:19:49 -0400

    added changes to index.php file

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 	</head>
 
 <body> 
-	<h1>PHP Test</h1>
-		<p><b>Kyle Jurassic is a god among machines</b></p>
+	<h1>Lav 9 - Building Applications from Source</h1>
+		<p><b>Changing the index file to test that s2i is working</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
